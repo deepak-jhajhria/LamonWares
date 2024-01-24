03b0af9f955b61701897f40aa9f5fd5d7acc26ef
Deepak jaat <deepakjhajhria4657@gmail.com>
2024-01-24 16:31:36 +0530

    UpdateNav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,12 @@
-"use client";
 import logo from '../assets/images/logo.webp'
 import { PhoneCallIcon } from "./Icons"
 import { useState } from "react"
 import { Sling as Hamburger } from 'hamburger-react'
 import { NavLink } from "react-router-dom";
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 const NavBar = () => {
-    let NavLinks = "text-black leading-normal text-sm sm:text-[15px] font-semibold font-Poppins relative after:duration-300 after:absolute after:-bottom-1 transition duration-300 after:w-0 hover:after:w-[80%] after:h-[1px] after:rounded-full after:bg-[#B00000] after:left-[10%]"
+    let NavLinks = "text-black link leading-normal text-sm sm:text-[15px] font-semibold font-Poppins relative after:duration-300 after:absolute after:-bottom-1 transition duration-300 after:w-0 hover:after:w-[80%] after:h-[1px] after:rounded-full after:bg-[#B00000] after:left-[10%]"
     const [isOpen, setOpen] = useState(false);
     if (isOpen) { document.body.classList.add("overflow-hidden") }
     else {
@@ -15,18 +16,39 @@ const NavBar = () => {
         setOpen(false)
         document.body.classList.remove("overflow-hidden")
     }
+    // useGSAP(()=>{
+    //     gsap.from(".logo",{y:300,opacity:0})
+    //     // gsap.from(".link" ,{stagger: 0.1 ,y:100})
+    // })
+    window.onscroll = function () { scrollFunction() };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            document.getElementById("navbar").classList.remove("sm:py-10")
+            document.getElementById("navbar").classList.add("py-5")
+            document.getElementById("nav").classList.add("fixed")
+            document.getElementById("nav").classList.add("-translate-x-1/2")
+            
+        } else {
+            document.getElementById("nav").classList.remove("-translate-x-1/2")
+            document.getElementById("nav").classList.remove("fixed")
+            document.getElementById("navbar").classList.remove("py-5")
+            document.getElementById("navbar").classList.add("sm:py-10")
+            document.getElementById("navbar").classList.add("py-5")
+        }
+    }
     return (
-        <nav className=" bg-white z-50">
-            <div className="container max-w-[1220px] py-10">
+        <nav id='nav'  className=" bg-white duration-300 z-40 left-1/2 w-full">
+            <div id='navbar' className="container max-w-[1220px] py-5 sm:py-10 z-40 bg-white">
                 <div className="flex w-full justify-between">
                     <div className="flex items-center gap-5 sm:gap-10 lg:gap-16">
-                        <a href="http://"><img className="max-sm:max-w-[70px]" src={logo} alt="logo" /></a>
+                        <a href="http://"><img className="max-sm:max-w-[70px] logo" src={logo} alt="logo" /></a>
                         <p className=" text-black leading-normal text-sm sm:text-[15px] font-semibold font-Poppins relative after:absolute after:-bottom-1 after:w-[80%] after:h-[2px] after:rounded-full after:bg-[#B00000] after:left-[10%]">Our Services</p>
                     </div>
-                    <div className={`${isOpen ? "right-0" : "-right-full"} flex items-center gap-[55px] mobileview lg:justify-between lg:ml-20 xl:ml-[135px] lg:w-[60%] xl:w-[67%]`}>
+                    <div className={`${isOpen ? "right-0" : "-right-full"} flex items-center gap-[55px] mobileview lg:justify-between lg:ml-20 xl:ml-[135px] lg:w-[60%] xl:w-[67%] z-50`}>
                         <ul className="flex max-lg:flex-col items-center gap-8">
-                            <li><NavLink to={"/about"} onClick={RemoveOverflow}  className={`${NavLinks}`}>About</NavLink></li>
-                            <li><NavLink to={"/blogs"} onClick={RemoveOverflow}  className={`${NavLinks}`}>Blog&News</NavLink></li>
+                            <li><NavLink to={"/about"} onClick={RemoveOverflow} className={`${NavLinks}`}>About</NavLink></li>
+                            <li><NavLink to={"/blogs"} onClick={RemoveOverflow} className={`${NavLinks}`}>Blog&News</NavLink></li>
                             <li><NavLink to={"/contact"} onClick={RemoveOverflow} className={`${NavLinks}`}>Contact</NavLink></li>
                         </ul>
                         <ul className="flex max-xs:flex-col items-center gap-8 xs:ml-10">
--- src/components/Preloder.jsx
+++ src/components/Preloder.jsx
@@ -1,7 +1,7 @@
 const Preloder = () => {
     return (
         <div className='bg-white min-h-screen w-full flex justify-center items-center'>
-            <div class="loader w-12 h-12 relative -translate-x-6 after:absolute after:w-full after:h-full after:rounded-full after:mix-blend-multiply after:bg-[#B00000] before:absolute before:w-full before:h-full before:rounded-full before:mix-blend-multiply before:bg-[#E8B3B3]"></div>
+            <div className="loader w-12 h-12 relative -translate-x-6 after:absolute after:w-full after:h-full after:rounded-full after:mix-blend-multiply after:bg-[#B00000] before:absolute before:w-full before:h-full before:rounded-full before:mix-blend-multiply before:bg-[#E8B3B3]"></div>
         </div>
     )
 }
